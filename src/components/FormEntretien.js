4e7e52e1b7adba76bacbe411c22d0df2680a1dbb
Lucas HR <lucashenintsoarandriamiarisoa@gmail.com>
2024-01-19 22:53:18 +0300

    commit 1

---
+++ src/components/FormEntretien.js
@@ -0,0 +1,104 @@
+import React,{ useState } from 'react';
+
+
+const FormEntretien = () => {
+    //style
+    const container = {
+        margin : '50px 0px',
+        border : 'solid',
+        borderWidth : '1px',
+        borderRadius : '5px',
+        borderColor : '#eaeaea',
+        width : 500,
+        padding : '30px 40px',
+        boxShadow : '0px 5px 5px #d7d5d3',
+       
+    }
+
+    const styleInput = {
+        border : 0,
+        background:'transparent',
+        padding : '10px 10px',
+        borderBottom : 'solid',
+        borderColor: ' #d7d5d3',
+        borderBottomWidth : '1px',
+        display : 'block',
+        margin : '20px 0px',
+        width : '100%',
+    }
+
+   
+    const listRegion = [
+        {id:1,nom:'Tana'},
+        {id:2,nom:'Tamatave'},
+        {id:3,nom:'Tulear'},
+        {id:4,nom:'Fianarantsoa'},
+        {id:5,nom:'Diego'}
+    ]
+
+    const listCommune = [
+        {region:"Tana", commune:['Ambaravarankazo','Ambatolampy']},
+        {region:"Tamatave",commune:['Marolambo','Mahanoro']},
+        {region:"Tulear",commune:['Ihosy','Renohira']},
+        {region:"Fianarantsoa",commune:['Isada','Ivory']},
+        {region:"Diego",commune:['Cap Diego','Be Manondrobe']}
+    ]
+
+    const [inputs, setInputs] = useState({
+        nom : '',
+        prenom : '',
+        region : '',
+        commune : '',
+        date : '',
+    });
+
+
+    const handleChange = (event) => {
+        const {name,value} = event.target;
+        setInputs({...inputs, [name]: value})
+    }
+
+    const validate = (event) => {
+        event.preventDefault();
+        console.log('Nom : ',inputs.nom)
+        console.log('Preom : ',inputs.prenom)
+        console.log('Region : ',inputs.region)
+        console.log('Commune : ',inputs.commune)
+        console.log('Date : ',inputs.date)
+    }
+
+    return (
+        <div style={container}>
+            <form onSubmit={validate}> 
+                <input type="text" placeholder='Nom' name="nom"  value={inputs.nom} onChange={handleChange} style={styleInput}/>
+                <input type="text" placeholder='Prenom' name="prenom" value={inputs.prenom} onChange={handleChange} style={styleInput}/>
+                <select value={inputs.region} onChange={handleChange} name='region' style={styleInput}>
+                    {
+                        listRegion.map((region) => {
+                            return <option value={region.nom} key={region.id}>{region.nom}</option>
+                        })
+                    }
+                </select>
+                <select value={inputs.commune} name='commune' onChange={handleChange} style={styleInput}>
+                    {
+                        listCommune.map((communes) => {
+                            if (inputs.region == communes.region){
+                                return communes.commune.map((c,index) => (
+                                    <option key={index}>{c}</option>
+                                ))
+                            }
+                            
+                        })
+                    }
+                </select>
+                <input type="date" name="date" style={styleInput} onChange={handleChange}/>
+                <input type="submit" value="Envoyer"/>
+            </form> 
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default FormEntretien;
